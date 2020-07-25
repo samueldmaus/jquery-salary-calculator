0692306bc5706cc3234eed5a8c3d392c6888f2a2
Sam Maus <samueldmaus@gmail.com>
2020-07-25 13:50:14 -0500

    added addToEmployeeTable function to add employees to DOM

--- client.js
+++ client.js
@@ -2,10 +2,42 @@ console.log('gg')
 
 $(document).ready(readyNow);
 
+let employeeArray = [];
+
 function readyNow() {
     $('#submitButton').on('click', addToEmployeeTable)
 }
 
 function addToEmployeeTable() {
     
+    let firstName = $('#employeeFirstName').val();
+    let lastName = $('#employeeLastName').val();
+    let employeeID = $('#employeeID').val();
+    let employeeTitle = $('#employeeTitle').val();
+    let employeeAnnualSalary = $('#employeeSalary').val();
+    let newEnteredEmployee = {
+        fName: firstName,
+        lName: lastName,
+        id: employeeID,
+        title: employeeTitle,
+        salary: employeeAnnualSalary,
+    }
+    employeeArray.push(newEnteredEmployee);
+    for(let i = 0; i < employeeArray.length; i++) {
+        $('#employeeInformation').append(
+            `<tr>
+                <td>${employeeArray[i].fName}</td>
+                <td>${employeeArray[i].lName}</td>
+                <td>${employeeArray[i].id}</td>
+                <td>${employeeArray[i].title}</td>
+                <td>${employeeArray[i].salary}</td>
+                <td><button id="deleteButton>Delete</button></td>
+            </tr>`
+        )
+    }
+    $('#employeeFirstName').val('');
+    $('#employeeLastName').val('');
+    $('#employeeID').val('');
+    $('#employeeTitle').val('');
+    $('#employeeSalary').val('');
 }
